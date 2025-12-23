0f0dcb87c53aab5206dea3af9e2d83547e62c193
Jose Antequera <janteq03@gmail.com>
2025-12-23 19:47:06 -0300

    se agrego admin

--- viajes/admin.py
+++ viajes/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import Destino, Viajero, Viaje
 
-# Register your models here.
+
+@admin.register(Destino)
+class DestinoAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'pais')
+    search_fields = ('nombre', 'pais')
+
+
+@admin.register(Viajero)
+class ViajeroAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'email')
+    search_fields = ('nombre', 'email')
+
+
+@admin.register(Viaje)
+class ViajeAdmin(admin.ModelAdmin):
+    list_display = ('viajero', 'destino', 'fecha')
+    list_filter = ('fecha', 'destino')
+    search_fields = ('viajero__nombre', 'destino__nombre')
+    date_hierarchy = 'fecha'
